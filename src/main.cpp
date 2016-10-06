24e9eb2e914056674451a4a21441ccd917f03eb8
Jonathan Hickford <jonathan.hickford@gmail.com>
2016-10-06 08:00:54 +0100

    Added https call to pushover

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
  */
 #include "Arduino.h"
 #include <ESP8266WiFi.h>
-#include <PubSubClient.h>
+#include <WiFiClientSecure.h>
 #include "config.h"
 
 void setup()
@@ -17,7 +17,7 @@ void setup()
   Serial.println();
   WiFi.begin(Config::network, Config::password);
 
-  Serial.print("Connecting");
+  Serial.print("Connecting to wifi");
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
@@ -25,7 +25,7 @@ void setup()
   }
   Serial.println();
 
-  Serial.print("Connected, IP address: ");
+  Serial.print("Connected to wifi, IP address: ");
   Serial.println(WiFi.localIP());
 }
 
@@ -39,4 +39,66 @@ void loop()
   digitalWrite(LED_BUILTIN, LOW);
    // wait for a second
   delay(1000);
+}
+
+
+
+void pushover() {
+  const char* host = "api.pushover.net";
+  const char* fingerprint = "27 41 80 CE F4 46 C9 02 F6 8C C0 D1 CC 72 3F D2 43 FA 35 0D";
+  const int httpsPort = 443;
+
+  WiFiClientSecure client;
+  Serial.print("Connecting to ");
+  Serial.println(host);
+
+  if(!client.connect(host, httpsPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  if (client.verify(fingerprint, host)) {
+    Serial.println("certificate matches");
+  } else {
+    Serial.println("certificate doesn't match");
+  }
+
+  String url = "/1/messages.json";
+  Serial.print("requesting URL: ");
+  Serial.println(url);
+
+  String content =  "token=" + String(Config::pushover_token) +
+   "&user=" + String(Config::pushover_user) +
+   "&title=" + "Study" +
+   "&message" + "Come+to+the+study";
+
+  client.print(String("POST ") + url +
+               "Host: " + host + "\r\n" +
+               "Content-Type: application/x-www-form-urlencoded\r\n\r\n" +
+               "Content-Length: " + content.length() + "\r\n" +
+               content + "\r\n"
+              );
+               Serial.println("request sent");
+
+  while (client.connected()) {
+    String line = client.readStringUntil('\n');
+    if (line == "\r") {
+      Serial.println("headers received");
+      break;
+    }
+  }
+  String line = client.readStringUntil('\n');
+   if (line.startsWith("{\"status\":1")) {
+     Serial.println("Message sent successfull!");
+   } else {
+     Serial.println("Message failure");
+   }
+   Serial.println("reply was:");
+   Serial.println("==========");
+   Serial.println(line);
+   Serial.println("==========");
+   Serial.println("closing connection");
+
+
+
 }
